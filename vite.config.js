1c82429b8afdf386fadb7b6540651014b42ccf87
dawood <facultystudent05@gmail.com>
2025-11-28 11:26:41 +0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,37 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     lib: {
+//       entry: 'src/main.tsx',
+//       name: 'PricingWidget',
+//       fileName: 'widget',
+//     },
+//     rollupOptions: {
+//       output: {
+//         globals: {
+//           react: 'React',
+//           'react-dom': 'ReactDOM',
+//         },
+//       },
+//     },
+//   },
+// });
+
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ← VERY IMPORTANT
   build: {
-    lib: {
-      entry: 'src/main.tsx',
-      name: 'PricingWidget',
-      fileName: 'widget',
-    },
+    outDir: 'dist',
     rollupOptions: {
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
+      input: 'index.html', // ← THIS IS THE KEY
     },
   },
-});
+})
